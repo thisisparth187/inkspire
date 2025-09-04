50702fd39977946188f29fadc8a8d87f2c8d0c7d
Parth <140729488+thisisparth187@users.noreply.github.com>
2025-09-04 18:34:20 +0530

    Deploy issues

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://inkspire-6tsq.onrender.com/api/blogs",
+  baseURL: "https://inkspire-6tsq.onrender.com/api",
 });
 
 // Automatically attach token if available
--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth"; // match your backend
+const API_URL = "https://inkspire-6tsq.onrender.com/api/auth"; // match your backend
 
 // Register user
 export const register = async (userData) => {
