2a3bdfd4343acbe2fa4c52d008a139901e59dbbc
Parth <140729488+thisisparth187@users.noreply.github.com>
2025-09-04 17:58:22 +0530

    Deploy commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://inkspire-6tsq.onrender.com",
 });
 
 // Automatically attach token if available
